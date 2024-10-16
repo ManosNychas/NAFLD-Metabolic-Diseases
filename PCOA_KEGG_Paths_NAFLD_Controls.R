ff81da9678d397e1f2803b00e262e22975b023ca
ManosNychas <36704011+ManosNychas@users.noreply.github.com>
2024-10-16 15:57:38 +0300

    NMDS_KEGG_Paths_NAFLD_Controls.R

--- PCOA_KEGG_Paths_NAFLD_Controls.R
+++ PCOA_KEGG_Paths_NAFLD_Controls.R
@@ -11,7 +11,7 @@ library(ggsignif)
 library(textshape)
 
 ##Load data
-load("~/Documents/Metabolic_Diseases/Updated_Human3/NAFLD_O_L/R_Datasets/Updated_NAFLD_O_L_KEGGPaths_CPM_Clinical_Data.RData")
+load("Documents/Phd/HKI_PC/Metabolic_Diseases/Updated_Human3/NAFLD_O_L/R_Datasets/Updated_NAFLD_O_L_KEGGPaths_CPM_Clinical_Data.RData")
 
 ##### Data Prep ####
 
@@ -33,107 +33,116 @@ full_features[is.na(full_features)] <- 0
 
 
 
-df <- read.csv("~/Documents/Metabolic_Diseases/Updated_Human3/Common_Metadata_Sara/FINAL_metadata_all_samples_Updated_FLI_FIB4.csv")
+df <- read.csv("Documents/Phd/HKI_PC/Metabolic_Diseases/Updated_Human3/Common_Metadata_Sara/FINAL_metadata_all_samples_Updated_FLI_FIB4.csv")
 
 
 
 full_metadata  <- df  %>% .[which(df$X %in% rownames(full_features)),]
 full_metadata = full_metadata[order(match(full_metadata$X,rownames(full_features))),] 
 full_metadata <- full_metadata %>%
-   dplyr::select(X,
-                 Age.demographic_variable,
-                 Gender.demographic_variable,
-                 BMI.body_weight_variables,
-                 HOMAIR.glucose_insulin,
-                 Systolic_pressure.cardiac_variables
-   ) %>% 
-   dplyr::rename(IDs = "X") %>% 
-   dplyr::rename(Age = "Age.demographic_variable") %>% 
-   dplyr::rename(BMI = "BMI.body_weight_variables") %>% 
-   dplyr::rename(Gender = "Gender.demographic_variable") %>% 
-   dplyr::rename(HOMA_IR = "HOMAIR.glucose_insulin") %>% 
-   dplyr::rename(SBP = "Systolic_pressure.cardiac_variables") %>% 
-   dplyr::mutate(Groups = factor(c(rep("NAFLD-O",nrow(NAFLD_O)),
-                                   rep("NAFLD-L",nrow(NAFLD_L)),
-                                   rep("Control-O",nrow(Control_O)),
-                                   rep("Control-L",nrow(Control_L))),
-                                 levels =c("NAFLD-O",
-                                           "NAFLD-L",
-                                           "Control-O",
-                                           "Control-L"
-                                 ))) %>% 
-   textshape::column_to_rownames(., "IDs") %>% 
-   .[complete.cases(.), ] 
+  dplyr::select(X,
+                Age.demographic_variable,
+                Gender.demographic_variable,
+                BMI.body_weight_variables,
+                HOMAIR.glucose_insulin,
+                Systolic_pressure.cardiac_variables
+  ) %>% 
+  dplyr::rename(IDs = "X") %>% 
+  dplyr::rename(Age = "Age.demographic_variable") %>% 
+  dplyr::rename(BMI = "BMI.body_weight_variables") %>% 
+  dplyr::rename(Gender = "Gender.demographic_variable") %>% 
+  dplyr::rename(HOMA_IR = "HOMAIR.glucose_insulin") %>% 
+  dplyr::rename(SBP = "Systolic_pressure.cardiac_variables") %>% 
+  dplyr::mutate(Groups = factor(c(rep("NAFLD-O",nrow(NAFLD_O)),
+                                  rep("NAFLD-L",nrow(NAFLD_L)),
+                                  rep("Control-O",nrow(Control_O)),
+                                  rep("Control-L",nrow(Control_L))),
+                                levels =c("NAFLD-O",
+                                          "NAFLD-L",
+                                          "Control-O",
+                                          "Control-L"
+                                ))) %>% 
+  textshape::column_to_rownames(., "IDs") %>% 
+  .[complete.cases(.), ] 
 
 
 
 full_features =  full_features[which(rownames(full_features) %in% rownames(full_metadata)),]
 
 
-#### BC PCOA ####
+#### BC NMDS ####
 set.seed(123)
-BC_Comb = vegdist(full_features,method = "bray")
-PCOA = ape::pcoa(BC_Comb)
+
+B_Dist <- full_features %>% 
+  vegdist(.,method = "bray") 
+
+NMDS_3_BC <- 
+  B_Dist %>% 
+  metaMDS(.,k=3)
+
+
 full = cbind(full_features,full_metadata)
 
 
-PWA_BC_treat_NMDS_2 <- adonis2(BC_Comb ~  Age + Gender + BMI + HOMA_IR + SBP  + Groups, data = full, permutations = 999,by = "terms")
 
 
 
+PWA_BC_treat_NMDS_2 <- adonis2(B_Dist ~  Age + Gender + BMI + HOMA_IR + SBP  + Groups, 
+                               data = full, permutations = 999,by = "terms")
+
+
+Scores_NMDS <- NMDS_3_BC %>%  
+  vegan::scores(.) %>% 
+  as.data.frame() %>% 
+  dplyr::select(1,2)
 ##GG Plot
 
 ggplot_df <-
-   data.frame(PCoA_1 = PCOA$vectors[,1],PCoA_2 = PCOA$vectors[,2],Groups = full$Groups) %>% 
-   group_by(Groups) %>% 
-   summarise_each(funs(mean,sd,se=sd(.)/sqrt(n()))) %>%
-   rename(.,PCoA_1 = PCoA_1_mean,PCoA_2 = PCoA_2_mean,SE_1 = PCoA_1_se,SE_2 = PCoA_2_se) %>% 
-   mutate(Groups = gsub("Control-O","CTRL-NAFLD-O",Groups)) %>% 
-   mutate(Groups = gsub("Control-L","CTRL-NAFLD-L",Groups)) %>% 
-   mutate(Groups = factor(Groups, levels = c("NAFLD-O",
+  data.frame(PCoA_1 = Scores_NMDS$NMDS1, PCoA_2 = Scores_NMDS$NMDS2 ,Groups = full$Groups) %>% 
+  group_by(Groups) %>% 
+  summarise_each(funs(mean,sd,se=sd(.)/sqrt(n()))) %>%
+  rename(.,PCoA_1 = PCoA_1_mean,PCoA_2 = PCoA_2_mean,SE_1 = PCoA_1_se,SE_2 = PCoA_2_se) %>% 
+  mutate(Groups = gsub("Control-O","CTRL-NAFLD-O",Groups)) %>% 
+  mutate(Groups = gsub("Control-L","CTRL-NAFLD-L",Groups)) %>% 
+  mutate(Groups = factor(Groups, levels = c("NAFLD-O",
                                             "NAFLD-L",
                                             "CTRL-NAFLD-O",
                                             "CTRL-NAFLD-L")))
 
 
 
-
-
-PCOA$values$Eigenvalues[1]/sum(PCOA$values$Eigenvalues)
-PCOA$values$Eigenvalues[2]/sum(PCOA$values$Eigenvalues)
-
-
 pcoa_bc_beta_div <-ggplot(ggplot_df, aes(x=PCoA_1, y=PCoA_2,colour = Groups, label = Groups)) + 
-   geom_errorbar(aes(ymin=PCoA_2 - SE_2, ymax=PCoA_2 + SE_2),linetype = 1,size =1.5,width = 0.001) +
-   geom_errorbar(aes(xmin=PCoA_1 - SE_1, xmax=PCoA_1 + SE_1),linetype = 1,size =1.5,width = 0.001) +
+  geom_errorbar(aes(ymin=PCoA_2 - SE_2, ymax=PCoA_2 + SE_2),linetype = 1,size =1.5,width = 0.001) +
+  geom_errorbar(aes(xmin=PCoA_1 - SE_1, xmax=PCoA_1 + SE_1),linetype = 1,size =1.5,width = 0.001) +
   geom_label(size = 8) + 
   scale_color_manual(values=c("#d86b6f","#6182a8", "#d86b6f", "#6182a8")) +
-   theme_classic() +
-   theme(plot.title = element_text(size = 14, hjust = 0.5),  text = element_text(size = 15),
-         legend.position="none",
-         axis.text.x=element_blank(), #remove x axis labels
-         axis.ticks.x=element_blank(), #remove x axis ticks
-         axis.text.y=element_blank(),  #remove y axis labels
-         axis.ticks.y=element_blank(),
-         panel.border = element_rect(colour = "black", fill=NA, size=2),
-         axis.title=element_text(size=20)) +
-   annotate("text", x = -0.03, y = 0.02, label = paste(("P value = "),PWA_BC_treat_NMDS_2$`Pr(>F)`[6]),size=8) +
-   annotate("text", x = -0.03, y = 0.018, label = paste(("R² = 0.006")),size=8) +
-   labs(x = "PCoA 1 68%",
-        y = "PCoA 2 14%")
+  theme_classic() +
+  theme(plot.title = element_text(size = 14, hjust = 0.5),  text = element_text(size = 15),
+        legend.position="none",
+        axis.text.x=element_blank(), #remove x axis labels
+        axis.ticks.x=element_blank(), #remove x axis ticks
+        axis.text.y=element_blank(),  #remove y axis labels
+        axis.ticks.y=element_blank(),
+        panel.border = element_rect(colour = "black", fill=NA, size=2),
+        axis.title=element_text(size=20)) +
+  annotate("text", x = -0.03, y = 0.02, label = paste(("P value = "),PWA_BC_treat_NMDS_2$`Pr(>F)`[6]),size=8) +
+  annotate("text", x = -0.0375, y = 0.025, label = paste(("R² = 0.006")),size=8) +
+  labs(x = "NMDS 1",
+       y = "NMDS 2")
 
+pdf("Documents/Phd/Metabolic_Diseases/Submissions/Cell/Supplementary Figures/S_Figure_2_Kegg.pdf", width = 15, height = 10)
+pcoa_bc_beta_div
+dev.off()
 
 ##Adonis Function for each single group vs another single group to test beta diversity between them
 
 adonis_new <- function(Metadata) {
   set.seed(123)
   
-  
-  
   Features =  full_features[which(rownames(full_features) %in% rownames(Metadata)),]
   
   new_BC_Comb = vegdist(Features,method = "bray")
-  new_PCOA = ape::pcoa(new_BC_Comb)
+  #new_PCOA = ape::pcoa(new_BC_Comb)
   new_full = cbind(Features,Metadata)
   
   
